79a24cc6edf166a3d3794fc78cac5305816ca2c7
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2016-08-18 12:51:32 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-admin-plugs',
+    version='1.0',
+    description='Plugabble apps that extend the django admin.',
+    author='Pushkar Paranjpe',
+    author_email='pushkar@metamatic.us',
+    url='https://github.com/pushkarparanjpe/django-admin-plugs/',
+    packages=['dash_google_analytics'],
+)
